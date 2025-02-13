6aee3cafd2a5819de68a2e5c225a50274f29f967
apiantonio <apiantonio.aa@gmail.com>
2025-02-13 23:02:01 +0100

    [1.0.5]

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='1.0.2',
     packages=find_packages(),
     description='Code of \"Data Structures and Algorithms in Python\"',
-    long_description="[1.0.4 - 2025-02-13]: KMP per tutte le occorrenze, commenti vari",
+    long_description="[1.0.5 - 2025-02-13]: KMP per tutte le occorrenze e per l'ultima, commenti vari",
     author='Michael T. Goodrich, Roberto Tamassia, Michael H. Goldwasser',
 )

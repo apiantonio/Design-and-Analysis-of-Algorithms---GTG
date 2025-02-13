6ef0c4e0c7c7525c49f64157154eb2f21a50af59
apiantonio <apiantonio.aa@gmail.com>
2025-02-13 23:04:22 +0100

    errata corrige [1.0.5]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TdP_collections',
-    version='1.0.2',
+    version='1.0.5',
     packages=find_packages(),
     description='Code of \"Data Structures and Algorithms in Python\"',
     long_description="[1.0.5 - 2025-02-13]: KMP per tutte le occorrenze e per l'ultima, commenti vari",
